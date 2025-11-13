232015d5664112cf179258ca9880c63f6ac431e8
yurei <167486448+yurei0903@users.noreply.github.com>
2025-11-14 07:39:36 +0900

    create 1 file and update 3 files

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,24 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import tailwindcss from "@tailwindcss/vite";
+import path from "path"; // ◀◀ 追加
+
+const repositoryName = "react-todo-app";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  
+    base: process.env.NODE_ENV === "production" ? `/${repositoryName}/` : "/",
+  build: {
+    rollupOptions: {
+      input: {
+        main: path.resolve(__dirname, "index.html"),
+        404: path.resolve(__dirname, "404.html"),
+      },
+    },
+  },
+
   server: {
     port: 3000,
     strictPort: false,
