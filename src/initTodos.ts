adc7ad60ea34c49b71ded856f0dd397589049642
yurei <167486448+yurei0903@users.noreply.github.com>
2025-11-19 19:07:08 +0900

    update initTodos.ts

--- src/initTodos.ts
+++ src/initTodos.ts
@@ -3,26 +3,10 @@ import { v4 as uuid } from "uuid"; // v4 を uuid という名前でインポー
 export const initTodos: Todo[] = [
   {
     id: uuid(), // UUID v4 を生成してIDにセット
-    name: "解析2の宿題",
+    name: "Sample Card A",
     isDone: false,
-    priority: 3,
+    priority: 4,
     url: null,
-    price: null,
-  },
-  {
-    id: uuid(),
-    name: "TypeScriptの勉強 (復習)",
-    isDone: false,
-    priority: 3,
-    url: null,
-    price: null,
-  },
-  {
-    id: uuid(),
-    name: "基礎物理学3の宿題",
-    isDone: false,
-    priority: 3,
-    url: "https://takeshiwada1980.github.io/Programming3-2025/lecture04.html",
-    price: null,
+    price: 33,
   },
 ];
